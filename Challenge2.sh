b90a6333dba87f9097bd82c5be7b029ae89af2c1
Cody Wishart <cody.wishart@gmail.com>
2021-03-03 20:14:44 -0600

    Create Challenge2.sh

---
+++ Challenge2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:                  Challenge2 
+# Class Name:                   Ops 301
+# Author Name:                  Cody Wishart
+# Date of latest revision:      3/3/21
+# Purpose:                      Use chmod to change permissions
+
+# Variables
+targetDir=""
+newPerm=""
+
+# Main
+echo "Enter target directory"
+read targetDir
+cd $targetDir
+echo "Enter new permission #"
+read newPerm
+chmod $newPerm ./*
+echo ""
+ls -al
