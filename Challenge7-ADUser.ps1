18ffa3fedfcb0e728a0218b03de631683b8050b8
Cody Wishart <cody.wishart@gmail.com>
2021-03-10 20:57:18 -0600

    Update and rename Cha to Challenge7-ADUser.ps1

--- Challenge7-ADUser.ps1
+++ Challenge7-ADUser.ps1
@@ -1,3 +1,11 @@
+<#
+  Script Name:                  Challenge1-ADUser
+  Class Name:                   Ops 301
+  Author Name:                  Cody Wishart
+  Date of latest revision:      3/10/21
+  Purpose:                      Add a new AD user 
+#>
+
 New-ADUser -Name "Franz Ferdinand" -Office "Springfield, OR" -OtherAttributes @{ 'title'="TPS Reporting Lead";
                                                                                  'mail'="ferdi@GlobeXpower.com";
                                                                                  'company'="GlobeX USA";
