20672b304e383cc70bf500ff7bd20c45f24fc845
Cody Wishart <cody.wishart@gmail.com>
2021-03-11 23:16:58 -0600

    Create Challenge8-listPractice.py

---
+++ Challenge8-listPractice.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Challenge6-listPractice
+# Class Name:                   Ops 301
+# Author Name:                  Cody Wishart
+# Date of latest revision:      3/11/21
+# Purpose:                      Do cool things with a list var
+
+# Import Library
+import os
+
+# Declare list
+trueStory = ["Your", "Mom", "is", "a", "very", "nice", "lady", "probably.", " Who", "knows?"]
+
+# Main
+print (trueStory)
+print ("\n" + trueStory[3])
+print (trueStory[5:11])
+trueStory[6] = "onion"
+print (trueStory)
