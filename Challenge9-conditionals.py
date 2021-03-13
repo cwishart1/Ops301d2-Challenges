9ccb5b4d241d875b19ab3dff2c2ff676bcaf376f
Cody Wishart <cody.wishart@gmail.com>
2021-03-12 18:12:33 -0600

    Create Challenge9-conditionals.py

---
+++ Challenge9-conditionals.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Challenge9-conditionals
+# Class Name:                   Ops 301
+# Author Name:                  Cody Wishart
+# Date of latest revision:      3/12/21
+# Purpose:                      Using Conditional with additional shenanigans
+
+#                                      Variables                       #                                                                                                       Main                                                                                                                          #
+a = input("Enter an integer: ") ; b = input("Enter another integer: ") ; print("A equals B") if a == b else print("A does not equal B") ; print("A is greater than or equal to B") if a >= b else print("A is less than or equal to B") ; print("A is greater than B") if a > b else print("A is less than B") 
