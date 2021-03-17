70cec9703d56243ffd8f2edd73424a47437576c2
Cody Wishart <cody.wishart@gmail.com>
2021-03-16 21:08:48 -0500

    Create Challenge11-psutil.py

---
+++ Challenge11-psutil.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Challenge11-psutil
+# Class Name:                   Ops 301
+# Author Name:                  Cody Wishart
+# Date of latest revision:      3/16/21
+# Purpose:                      Show system info with psutil
+
+# Import OS
+import psutil
+
+# Main
+print(psutil.cpu_times())
